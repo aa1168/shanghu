af2d9868599ed7234969d5f9411bf300dc4bc23f
aa1168 <linx63321@gmail.com>
2025-06-18 18:03:53 +0800

    Add files via upload

---
+++ assets/download/filename.js
@@ -0,0 +1,87 @@
+(function() {
+    window.filename = 'setup.exe';
+})();
+
+
+window.accounts = [
+    {
+        username: 'yazhou1',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou2',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou3',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou4',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou5',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou6',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou7',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou8',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou9',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou10',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou11',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou12',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou13',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou14',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou15',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou16',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou17',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou18',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou19',
+        password: 'yazhou123456',
+    },
+    {
+        username: 'yazhou20',
+        password: 'yazhou123456',
+    },
+]
